a7ab22f0b24670144cd46c510ff180b8afbc3459
yousef <yousefazmy102@yahoo.com>
2026-03-11 02:44:10 +0200

    add 3 models(Student_Enrollment,Session,Student_Attendance) and configrations

---
+++ EraasoftAcademy/DataAccess/Configurations/SessionConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using EraasoftAcademy.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EraasoftAcademy.DataAccess.Configurations
+{
+    public class SessionConfiguration : IEntityTypeConfiguration<Session>
+    {
+        public void Configure(EntityTypeBuilder<Session> builder)
+        {
+            builder.HasKey(s => s.SessionId);
+
+            builder.Property(s => s.RoomNUM)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            builder.Property(s => s.SessionDate)
+                .IsRequired();
+
+            //builder.HasOne(s => s.Course)
+            //    .WithMany(c => c.Sessions)
+            //    .HasForeignKey(s => s.CourseId)
+            //    .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ EraasoftAcademy/Models/StudentAttendance.cs
@@ -0,0 +1,16 @@
+﻿namespace EraasoftAcademy.Models
+{
+    public class StudentAttendance
+    {
+        public int AttendanceId { get; set; } // primary key for the StudentAttendance entity
+        //public Session Session { get; set; } = default!; // Session relationship
+        public int SessionId { get; set; } // foreign key to the Session entity
+        public StudentEnrollment Enrollment { get; set; } = default!; // Enrollment relationship
+        public int EnrollmentId { get; set; } // foreign key to the StudentEnrollment entity
+        //public Student Student { get; set; } = default!; // Student relationship
+        public int StudentId { get; set; } // foreign key to the Student entity
+        public string Status { get; set; } = null!; // attendance status (Present, Absent, Late)
+
+        public string? Note { get; set; } // optional note for the attendance record
+    }
+}
